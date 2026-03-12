a9e5e898b5539a51f8289a553b92feb1a98b4888
袴田 大貴 <hakamata_daiki@cyberagent.co.jp>
2026-03-12 20:22:17 +0900

    add tracking capabilities to FakePlayer

--- Packages/AudioConductor/Tests/Runtime/Core/Fakes/FakePlayer.cs
+++ Packages/AudioConductor/Tests/Runtime/Core/Fakes/FakePlayer.cs
@@ -15,6 +15,25 @@ namespace AudioConductor.Core.Tests.Fakes
         public int StopCount { get; private set; }
         public bool StopCalled => StopCount > 0;
         public float CategoryVolume { get; private set; } = 1f;
+
+        public int PlayCount { get; private set; }
+        public int PauseCount { get; private set; }
+        public int ResumeCount { get; private set; }
+        public int SetupCount { get; private set; }
+        public int ManualUpdateCount { get; private set; }
+        public float Volume { get; private set; }
+        public float Pitch { get; private set; }
+        public float MasterVolume { get; private set; }
+        public int CurrentSample { get; private set; }
+        public float LastDeltaTime { get; private set; }
+        public float SetupVolume { get; private set; }
+        public float SetupPitch { get; private set; }
+        public bool SetupIsLoop { get; private set; }
+        public int SetupStartSample { get; private set; }
+        public int SetupLoopStartSample { get; private set; }
+        public int SetupEndSample { get; private set; }
+        public Action? LastStopAction { get; private set; }
+        public Action? LastEndAction { get; private set; }
         public bool IsPlaying { get; set; }
         public bool IsPaused { get; set; }
         public uint ActiveFadeId { get; set; }
@@ -29,12 +48,20 @@ namespace AudioConductor.Core.Tests.Fakes
             CategoryId = categoryId;
             if (clip != null)
                 ClipSamples = clip.samples;
+            SetupCount++;
+            SetupVolume = volume;
+            SetupPitch = pitch;
+            SetupIsLoop = isLoop;
+            SetupStartSample = startSample;
+            SetupLoopStartSample = loopStartSample;
+            SetupEndSample = endSample;
         }
 
         public void Play()
         {
             IsPlaying = true;
             IsPaused = false;
+            PlayCount++;
         }
 
         public void Restart()
@@ -48,6 +75,7 @@ namespace AudioConductor.Core.Tests.Fakes
             {
                 IsPlaying = false;
                 IsPaused = true;
+                PauseCount++;
             }
         }
 
@@ -57,6 +85,7 @@ namespace AudioConductor.Core.Tests.Fakes
             {
                 IsPlaying = true;
                 IsPaused = false;
+                ResumeCount++;
             }
         }
 
@@ -74,11 +103,12 @@ namespace AudioConductor.Core.Tests.Fakes
 
         public float GetVolume()
         {
-            return 1f;
+            return Volume;
         }
 
         public void SetVolume(float volume)
         {
+            Volume = volume;
         }
 
         public float GetActualPitch()
@@ -88,28 +118,32 @@ namespace AudioConductor.Core.Tests.Fakes
 
         public float GetPitch()
         {
-            return 1f;
+            return Pitch;
         }
 
         public void SetPitch(float pitch)
         {
+            Pitch = pitch;
         }
 
         public void AddStopAction(Action onStop)
         {
+            LastStopAction = onStop;
         }
 
         public void AddEndAction(Action onEnd)
         {
+            LastEndAction = onEnd;
         }
 
         public int GetCurrentSample()
         {
-            return 0;
+            return CurrentSample;
         }
 
         public void SetCurrentSample(int sample)
         {
+            CurrentSample = sample;
         }
 
         public void SetVolumeFade(float fade)
@@ -119,6 +153,7 @@ namespace AudioConductor.Core.Tests.Fakes
 
         public void SetMasterVolume(float volume)
         {
+            MasterVolume = volume;
         }
 
         public void SetCategoryVolume(float volume)
@@ -128,6 +163,8 @@ namespace AudioConductor.Core.Tests.Fakes
 
         public void ManualUpdate(float deltaTime)
         {
+            ManualUpdateCount++;
+            LastDeltaTime = deltaTime;
         }
 
         public void ResetState()
@@ -139,6 +176,24 @@ namespace AudioConductor.Core.Tests.Fakes
             StopCount = 0;
             CategoryVolume = 1f;
             VolumeFade = 1f;
+            PlayCount = 0;
+            PauseCount = 0;
+            ResumeCount = 0;
+            SetupCount = 0;
+            ManualUpdateCount = 0;
+            Volume = 0f;
+            Pitch = 0f;
+            MasterVolume = 0f;
+            CurrentSample = 0;
+            LastDeltaTime = 0f;
+            SetupVolume = 0f;
+            SetupPitch = 0f;
+            SetupIsLoop = false;
+            SetupStartSample = 0;
+            SetupLoopStartSample = 0;
+            SetupEndSample = 0;
+            LastStopAction = null;
+            LastEndAction = null;
         }
     }
 }
